a8d1cd8e43d053646a6be3d9f50ca0adc0193d59
luis <luisfernandoe2018@gmail.com>
2022-08-23 22:10:09 -0500

    Clase empresa

---
+++ Empresa.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Empresa {
+    String nombre;
+    String direccion;
+    int telefono;
+    int nit;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
+}
