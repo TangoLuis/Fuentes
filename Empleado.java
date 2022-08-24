92fa813c4b3f0a12ca67af70cad153c2f4917d0c
luis <luisfernandoe2018@gmail.com>
2022-08-23 22:12:00 -0500

    clases

---
+++ Empleado.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Empleado {
+    String nombre;
+    String correoElectronico;
+    String empresaPertenece;
+    String rolEmpleado;
+
+
+}
---
+++ ProyectoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+	Empresa empresa1 = new Empresa();
+
+	empresa1
+
+}
