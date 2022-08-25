141ca0bbb0dda9465bfc50ed86f4d81fdda2525f
Edwin López <coste.jj@hotmail.com>
2022-08-24 20:32:57 -0500

    Proyecto Dev_mintic

---
+++ demo/src/main/java/com/example/demo/services/MovimientoDinero.java
@@ -0,0 +1,12 @@
+package com.example.demo.services;
+
+import lombok.Data;
+
+@Data
+public class MovimientoDinero {
+    private float monto;
+    private String conceptMovimiento;
+    public void registrarUsuario(){
+
+    }
+}
